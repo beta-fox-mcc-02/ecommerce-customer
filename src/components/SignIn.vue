b57d15debbb138a9f8898482a946f74eeabe6229
rahmandihamzah <rahmandi.dev@gmail.com>
2020-02-26 09:13:36 +0700

    sign in success get token

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="signIn-area">
     <div class="card card-body card-signIn">
-      <form>
+      <form @submit.prevent="signIn">
         <div class="form-group">
           <label for="email">Email: </label>
-          <input type="email" class="form-control" id="emial" placeholder="email" required>
+          <input type="email" class="form-control" id="emial" placeholder="email" required v-model="email">
         </div>
         <div class="form-group">
           <label for="password">Password: </label>
-          <input type="password" class="form-control" id="password" placeholder="password" required>
+          <input type="password" class="form-control" id="password" placeholder="password" required v-model="password">
         </div>
         <button class="btn btn-dark" type="submit">Sign In</button>
       </form>
@@ -18,11 +18,35 @@
 </template>
 
 <script>
+import axios from '../config/axios'
+
 export default {
   name: 'SignIn',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
   methods: {
     closeSignInForm () {
       this.$router.go(-1)
+    },
+    signIn () {
+      axios({
+        method: 'POST',
+        url: '/users/signIn',
+        data: {
+          email: this.email,
+          password: this.password
+        }
+      })
+        .then(({ data }) => {
+          localStorage.setItem('token', data.token)
+        })
+        .catch(({ response }) => {
+          console.log(response)
+        })
     }
   }
 }
@@ -33,6 +57,7 @@ export default {
   position: absolute;
   right: 0;
   display: flex;
+  z-index: 10;
 }
 
 .card-signIn form {
