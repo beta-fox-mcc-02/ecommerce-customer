412865a4ab5ba73b021a6aaa459f79236fc05fb5
ninoloid <muhammadsatriaadiputra@gmail.com>
2020-02-27 11:06:09 +0700

    thankspage

--- src/components/MainCard.vue
+++ src/components/MainCard.vue
@@ -9,6 +9,7 @@
       style="width: 100%; text-align: left; background: none; border: none;"
       class="mb-2"
     >
+      <b-card style="position: absolute; top: 0; left: 0; width: 100%; background: rgba(0,0,0,.3); font-size: 1.2rem; color: white;">SPECIAL PROMO 15% OFF! Use voucher code "payday" to get this special discount.</b-card>
       <b-button href="" variant="primary" @click.prevent="toCatalog">CATALOG</b-button>
     </b-card>
   </div>
