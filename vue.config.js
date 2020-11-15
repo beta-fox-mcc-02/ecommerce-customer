e2c5cc74a1d451ebdcccae06cd4b1423a2bfb44d
ganangniki <ganangmanggolo@gmail.com>
2020-11-16 00:40:18 +0700

    change run port

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-   lintOnSave: process.env.NODE_ENV !== 'production'
- }
+  lintOnSave: process.env.NODE_ENV !== "production",
+  devServer: {
+    open: process.platform === "darwin",
+    host: "0.0.0.0",
+    port: 8080, // CHANGE YOUR PORT HERE!
+    https: false,
+    hotOnly: false,
+  },
+};
